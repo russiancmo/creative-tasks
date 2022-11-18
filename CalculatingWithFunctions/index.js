d00e738ae9a7b17227d2cdd78b81a38133f5e6a4
ligadobra <russiancmo@gmail.com>
2022-11-18 19:26:43 +0300

    calculation-with-functions

---
+++ CalculatingWithFunctions/index.js
@@ -0,0 +1,54 @@
+function zero(operation) {
+  return operation ? operation(0) : 0;
+}
+function one(operation) {
+  return operation ? operation(1) : 1;
+}
+function two(operation) {
+  return operation ? operation(2) : 2;
+}
+function three(operation) {
+  return operation ? operation(3) : 3;
+}
+function four(operation) {
+  return operation ? operation(4) : 4;
+}
+function five(operation) {
+  return operation ? operation(5) : 5;
+}
+function six(operation) {
+  return operation ? operation(6) : 6;
+}
+function seven(operation) {
+  return operation ? operation(7) : 7;
+}
+function eight(operation) {
+  return operation ? operation(8) : 8;
+}
+function nine(operation) {
+  return operation ? operation(9) : 9;
+}
+
+function plus(number) {
+  return (secondNumber) => {
+    return secondNumber + number;
+  };
+}
+
+function minus(number) {
+  return (secondNumber) => {
+    return secondNumber - number;
+  };
+}
+
+function times(number) {
+  return (secondNumber) => {
+    return secondNumber * number;
+  };
+}
+
+function dividedBy(number) {
+  return (secondNumber) => {
+    return Math.floor(secondNumber / number);
+  };
+}
